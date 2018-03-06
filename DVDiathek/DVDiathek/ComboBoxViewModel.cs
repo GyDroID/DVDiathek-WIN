5fa40dc8eb541861d427d726769c30be53e096bd
GyDroID <w0ch3n3ndk4tz3@gmail.com>
2018-03-06 16:50:59 +0100

    Erste Version
    Erste XAML Version

---
+++ DVDiathek/DVDiathek/ComboBoxViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVDiathek
+{
+    class ComboBoxViewModel
+    {
+        public List<string> MediaSection { get; set; }
+
+        public ComboBoxViewModel() => MediaSection = new List<string>()
+            {
+                "Filme",
+                "Serien",
+            };
+    }
+}
